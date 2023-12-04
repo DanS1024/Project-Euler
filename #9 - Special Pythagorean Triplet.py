2c4da59a4d2471bb25528a8a110b4fb94f40a6a3
DanS1024 <mcds606@gmail.com>
2023-12-04 13:28:34 +0000

    9

---
+++ #9 - Special Pythagorean Triplet.py
@@ -0,0 +1,9 @@
+N = 36
+
+for a in range(1, N // 2):
+    if N*N % (2*(N-a)) == 0:
+        b = N - N*N // (2*(N-a))
+        c = N - a - b
+        # print(a, b, c)
+        print(a*b*c)
+        break
