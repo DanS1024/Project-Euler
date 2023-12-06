02a6d9296ea8506ae28c1a5bf4b1926d4b83de8e
DanS1024 <mcds606@gmail.com>
2023-12-06 20:48:37 +0000

    16

--- #16 - Power Digit Sum.py
+++ #16 - Power Digit Sum.py
@@ -5,11 +5,8 @@ carry = 0
 for i in range(N):
     for j in range(len(res)):
         res[j] = 2*res[j] + carry
-        if res[j] >= 10:
-            res[j] -= 10
-            carry = 1
-        else:
-            carry = 0
+        carry = res[j] // 10
+        res[j] %= 10
     if carry > 0:
         res.append(carry)
         carry = 0
